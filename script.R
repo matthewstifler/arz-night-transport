a01b670fb7f674fd46e790dc2b06c8dbdf4d4860
matthewstifler <mtwstflr@gmail.com>
2016-04-26 17:54:33 +0300

    finished converting coordinates

--- script.R
+++ script.R
@@ -3,10 +3,15 @@ require("rgdal", "dplyr")
 # get stops coordinates
 setwd("~/arz-night-transport")
 busstops = read.csv("bus-stops-10-06-15.csv")
+busstops = busstops[,c(2,4:6)]
+colnames(busstops) = c("id","name","easting","northing")
 
+coords <- cbind(Easting = as.numeric(as.character(busstops$easting)),
+                Northing = as.numeric(as.character(busstops$northing)))
 #setting grid systems, gotta convert ukgrid to latlong
 ukgrid = "+init=epsg:27700"
 latlong = "+init=epsg:4326"
+
 
 # load wards shapefiles
 # run ingress snippet (might fire back!) for id'ing in which shape what lies
